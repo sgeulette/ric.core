35a6967cb17fd986a1227296ab50c296afed43ff
Laurent Lasudry <lasudry@gmail.com>
2014-08-11 13:55:27 +0200

    Initial package structure

---
+++ setup.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+version = '0.1.dev0'
+
+from setuptools import setup, find_packages
+
+long_description = (
+    open('README.rst').read()
+    + '\n' +
+    'Contributors\n'
+    '============\n'
+    + '\n' +
+    open('CONTRIBUTORS.rst').read()
+    + '\n' +
+    open('CHANGES.rst').read()
+    + '\n')
+
+setup(name='ric.core',
+      version=version,
+      description='Core package for RIC',
+      long_description=long_description,
+      classifiers=[
+          "Environment :: Web Environment",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2.7",
+          "Framework :: Plone",
+          "Framework :: Plone :: 4.2",
+          "Framework :: Plone :: 4.3",
+      ],
+      keywords='',
+      author='Affinitic',
+      author_email='support@lists.affinitic.be',
+      url='https://github.com/affinitic/ric.core',
+      license='gpl',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'Plone',
+          # -*- Extra requirements: -*-
+      ],
+      extras_require={
+          'test': [
+              'plone.app.robotframework',
+          ]
+      },
+      entry_points={},
+)
