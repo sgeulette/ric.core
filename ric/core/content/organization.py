4e59f4c03dbcc06019f422e367502efbcb4d1dad
FBruynbroeck <francois@affinitic.be>
2014-08-14 14:09:33 +0200

    Add cotisation field Affinitic refs #5887

--- ric/core/content/organization.py
+++ ric/core/content/organization.py
@@ -1,7 +1,10 @@
+# encoding: utf-8
 import z3c.form
 
+from AccessControl import getSecurityManager
 from five import grok
 from plone.directives import dexterity
+from plone.directives import form
 from plone.z3cform.fieldsets import extensible
 from plone.z3cform.fieldsets.interfaces import IFormExtender
 from zope import schema
@@ -12,9 +15,17 @@ from zope.interface import implements
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 
 from collective.contact.core.content.organization import IOrganization
+from collective.z3cform.datagridfield import DataGridFieldFactory, DictRow
 
 
-class IOrganizationCustom(Interface):
+class ICotisationRow(form.Schema):
+    annee = schema.TextLine(title=u"Année",
+                            required=True)
+    versement = schema.Bool(title=u"Versement",
+                            required=True)
+
+
+class IOrganizationCustom(form.Schema):
 
     habitants = schema.TextLine(
         title=u"Nombre d'habitants",
@@ -29,6 +40,13 @@ class IOrganizationCustom(Interface):
     logiciels = schema.TextLine(
         title=u"Logiciels",
         required=True
+    )
+
+    cotisations = schema.List(
+        title=u"Cotisations",
+        value_type=DictRow(title=u"Cotisation",
+                           schema=ICotisationRow),
+        required=False,
     )
 
 
@@ -46,11 +64,16 @@ class OrganizationEditFormExtender(extensible.FormExtender):
         self.form = form
 
     def update(self):
+        loggedUser = getSecurityManager().getUser()
+        userRolesForParty = loggedUser.getRoles()
         if not IOrganizationCustom.providedBy(self.context):
             alsoProvides(self.context, IOrganizationCustom)
         self.remove('logo')
         self.remove('activity')
         self.add(z3c.form.field.Fields(IOrganizationCustom))
+        self.form.fields['cotisations'].widgetFactory = DataGridFieldFactory
+        if 'Manager' not in userRolesForParty:
+            self.remove('cotisations')
 
 
 class OrganizationAddForm(dexterity.AddForm):
@@ -67,8 +90,13 @@ class OrganizationAddFormExtender(extensible.FormExtender):
         self.form = form
 
     def update(self):
+        loggedUser = getSecurityManager().getUser()
+        userRolesForParty = loggedUser.getRoles()
         if not IOrganizationCustom.providedBy(self.context):
             alsoProvides(self.context, IOrganizationCustom)
         self.remove('logo')
         self.remove('activity')
         self.add(z3c.form.field.Fields(IOrganizationCustom))
+        self.form.fields['cotisations'].widgetFactory = DataGridFieldFactory
+        if 'Manager' not in userRolesForParty:
+            self.remove('cotisations')
