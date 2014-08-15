5fce21c1ad3bf34376f75a083bc5135fb2fda8be
Laurent Lasudry <lasudry@gmail.com>
2014-08-16 00:00:14 +0200

    Use behaviors instead of form extender
    This refs affinitic #5900

---
+++ ric/core/content/behaviors.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+
+from zope import schema
+from zope.interface import alsoProvides
+from plone.autoform.interfaces import IFormFieldProvider
+from plone.directives import form
+from plone.supermodel import model
+from collective.z3cform.datagridfield import DataGridField, DictRow
+
+from ric.core import RICMessageFactory as _
+
+
+class IPersonCustom(model.Schema):
+
+    form.read_permission(mailstatus='RIC: Administer website')
+    form.write_permission(mailstatus='RIC: Administer website')
+
+    mailstatus = schema.Bool(title=_(u"Statut e-mail"),
+                             required=True)
+
+    multimail = schema.List(title=_(u"Envoi mail"),
+                            required=False,
+                            value_type=schema.Choice(['contact cotisation',
+                                                      'formation']),
+                            )
+
+alsoProvides(IPersonCustom, IFormFieldProvider)
+
+
+class ICotisationRow(model.Schema):
+
+    annee = schema.TextLine(title=_(u"Année"),
+                            required=True)
+    versement = schema.Bool(title=_(u"Versement"),
+                            required=True)
+
+
+class IOrganizationCustom(model.Schema):
+
+    form.read_permission(cotisations='RIC: Administer website')
+    form.write_permission(cotisations='RIC: Administer website')
+    form.widget('cotisations', DataGridField)
+
+    habitants = schema.TextLine(
+        title=_(u"Nombre d'habitants"),
+        required=True
+    )
+
+    serveurs = schema.TextLine(
+        title=_(u"Serveurs"),
+        required=True
+    )
+
+    logiciels = schema.TextLine(
+        title=_(u"Logiciels"),
+        required=True
+    )
+
+    cotisations = schema.List(
+        title=_(u"Cotisations"),
+        value_type=DictRow(title=_(u"Cotisation"),
+                           schema=ICotisationRow),
+        required=False,
+    )
+
+
+alsoProvides(IOrganizationCustom, IFormFieldProvider)
--- ric/core/content/extender.py
+++ ric/core/content/extender.py
@@ -1,19 +1,12 @@
-# encoding: utf-8
+# -*- coding: utf-8 -*-
 
-from AccessControl import getSecurityManager
-from plone import api
-from plone.z3cform.fieldsets import extensible
-from plone.z3cform.fieldsets.interfaces import IFormExtender
 from zope.component import adapts
 from zope.interface import implements
 from zope.interface import Interface
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
-from z3c.form.field import Fields
 from z3c.form.interfaces import IEditForm, IAddForm, IForm
-from collective.z3cform.datagridfield import DataGridFieldFactory
-
-from ric.core.content.person import IPersonCustom
-from ric.core.content.organization import IOrganizationCustom
+from plone.z3cform.fieldsets import extensible
+from plone.z3cform.fieldsets.interfaces import IFormExtender
 
 
 class ContactFormExtender(extensible.FormExtender):
@@ -36,18 +29,9 @@ class ContactFormExtender(extensible.FormExtender):
         if self.form.portal_type == 'organization':
             self.remove('logo')
             self.remove('activity')
-            self.add(Fields(IOrganizationCustom))
-            self.form.fields['cotisations'].widgetFactory = DataGridFieldFactory
-            sm = getSecurityManager()
-            if not sm.checkPermission('RIC: Administer website', api.portal.get()):
-                self.remove('cotisations')
 
         elif self.form.portal_type == 'person':
             self.remove('gender')
             self.remove('person_title')
             self.remove('photo')
             self.form.groups[0].fields['IContactDetails.email'].field.required = True
-            self.add(Fields(IPersonCustom))
-            sm = getSecurityManager()
-            if not sm.checkPermission('RIC: Administer website', api.portal.get()):
-                self.remove('mailstatus')
