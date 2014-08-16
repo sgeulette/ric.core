62d95619d52e6f54e60117deef0b2a338e039acd
Laurent Lasudry <lasudry@gmail.com>
2014-08-16 15:39:50 +0200

    Add invalid email error viewlet This refs affinitic #5894

--- ric/core/viewlets/viewlets.py
+++ ric/core/viewlets/viewlets.py
@@ -1,16 +1,38 @@
+from zope.component import getMultiAdapter
 from plone.app.layout.viewlets.common import ViewletBase
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 
-class CotisationViewlet(ViewletBase):
+class RICViewletBase(ViewletBase):
+    """
+    """
+
+    def available():
+        pass
+
+
+class CotisationViewlet(RICViewletBase):
     render = ViewPageTemplateFile('cotisation.pt')
 
     def available(self):
-        return True
+        return False
 
 
-class ProfileViewlet(ViewletBase):
+class ProfileViewlet(RICViewletBase):
     render = ViewPageTemplateFile('profile.pt')
 
     def available(self):
-        return True
+        return False
+
+
+class EmailViewlet(RICViewletBase):
+    render = ViewPageTemplateFile('email.pt')
+    link = ""
+
+    def available(self):
+        person = getMultiAdapter((self.context, self.request),
+                                 name="get_person_for_user")()
+        if person and person.invalidmail == True:
+            self.link = person.absolute_url()
+            return True
+        return False
