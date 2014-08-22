6006c99dbac5a8b6bcde65830a8667a6e50657ec
FBruynbroeck <francois@affinitic.be>
2014-08-22 10:58:45 +0200

    Fix unicode to ascii for membrane userid Affinitic refs #5912

--- ric/core/membrane.py
+++ ric/core/membrane.py
@@ -5,6 +5,11 @@ class RICPersonMembraneUserAdapter(PersonMembraneUserAdapter):
 
     def getUserName(self):
         if self.context.userid:
+            if isinstance(self.context.userid, unicode):
+                # no need to use portal encoding for transitional
+                # encoding from
+                # unicode to ascii. utf-8 should be fine.
+                self.context.userid = self.context.userid.encode('utf-8')
             return self.context.userid
         else:
             return super(RICPersonMembraneUserAdapter, self).getUserName()
