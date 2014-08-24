eb9d564d8148f67d4ec061afbae3f4d0e83f5f56
Laurent Lasudry <lasudry@gmail.com>
2014-08-24 14:23:33 +0200

    Minor comment formatting

--- ric/core/membrane.py
+++ ric/core/membrane.py
@@ -6,9 +6,8 @@ class RICPersonMembraneUserAdapter(PersonMembraneUserAdapter):
     def getUserName(self):
         if self.context.userid:
             if isinstance(self.context.userid, unicode):
-                # no need to use portal encoding for transitional
-                # encoding from
-                # unicode to ascii. utf-8 should be fine.
+                # no need to use portal encoding for transitional encoding
+                # from unicode to ascii. utf-8 should be fine.
                 self.context.userid = self.context.userid.encode('utf-8')
             return self.context.userid
         else:
